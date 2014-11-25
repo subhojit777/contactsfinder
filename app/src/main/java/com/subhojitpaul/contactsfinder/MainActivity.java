147e30a441eb4bc19749a5ee4626b048ebe3cdbc
Subhojit Paul <subhojitpaul21@gmail.com>
2014-11-25 23:37:58 +0530

    Added MainActivity
    MainActivity will be the first activity which users sees when he opens
    the app.
    This app will have a dial like structure where user sets range.

---
+++ app/src/main/java/com/subhojitpaul/contactsfinder/MainActivity.java
@@ -0,0 +1,49 @@
+package com.subhojitpaul.contactsfinder;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends ActionBarActivity {
+    public final static String RANGE = "com.subhojitpaul.contactsfinder.RANGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Show friends location within the input range */
+    public void locateFriends(View view) {
+        Intent intent = new Intent(this, ShowMapActivity.class);
+        float range = 0.5f; // @todo This value will be user input.
+        intent.putExtra(RANGE, range);
+        startActivity(intent);
+    }
+}
