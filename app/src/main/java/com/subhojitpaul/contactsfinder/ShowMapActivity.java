283dbe31e48af7b96f171a0d2b8682f06e63de46
Subhojit Paul <subhojitpaul21@gmail.com>
2014-12-08 23:19:21 +0530

    Code using LocationListener
    Issue: #4

--- app/src/main/java/com/subhojitpaul/contactsfinder/ShowMapActivity.java
+++ app/src/main/java/com/subhojitpaul/contactsfinder/ShowMapActivity.java
@@ -8,6 +8,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,32 +16,34 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.ArrayList;
 
 public class ShowMapActivity extends Activity implements LocationListener {
-    private Double deviceLatitude;
-    private Double deviceLongitude;
+    private double deviceLatitude;
+    private double deviceLongitude;
     private LocationManager locationManager;
     private String provider;
     private GoogleMap map;
+    private ArrayList<double[]> locations;
+    private static final String TAG = "ContactsFinder";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_map);
         Intent intent = getIntent();
-        String range = intent.getStringExtra(MainActivity.RANGE);
 
-        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        showLocations();
+    }
 
+    public double[] getDeviceLocation() {
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         Criteria criteria = new Criteria();
         provider = locationManager.getBestProvider(criteria, false);
         Location location = locationManager.getLastKnownLocation(provider);
 
-        if (location != null) {
-            onLocationChanged(location);
-        }
+        return new double[] {location.getLatitude(), location.getLongitude()};
     }
 
     @Override
@@ -57,12 +60,20 @@ public class ShowMapActivity extends Activity implements LocationListener {
 
     @Override
     public void onLocationChanged(Location location) {
+        Log.d(TAG, "here");
         deviceLatitude = location.getLatitude();
         deviceLongitude = location.getLongitude();
 
-        Marker currentDeviceLocation = map.addMarker(new MarkerOptions().position(new LatLng(deviceLatitude, deviceLongitude)));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(deviceLatitude, deviceLongitude), 15));
-        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        double[] deviceLocation = new double[] {deviceLatitude, deviceLongitude};
+        double[] anotherLocation = getAnotherLocation();
+
+        locations.add(anotherLocation);
+        locations.add(deviceLocation);
+
+        showLocations();
+
+        //map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(deviceLatitude, deviceLongitude), 15));
+        //map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
     }
 
     @Override
@@ -78,5 +89,24 @@ public class ShowMapActivity extends Activity implements LocationListener {
     @Override
     public void onProviderDisabled(String s) {
         // Auto-generated stub.
+    }
+
+    public double[] getAnotherLocation() {
+        return new double[] {22.569165, 88.433597};
+    }
+
+    public void showLocations() {
+        ArrayList locations = new ArrayList();
+        locations.add(getDeviceLocation());
+        locations.add(getAnotherLocation());
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        if (map != null) {
+            for (int i = 0; i < locations.size(); i++) {
+                locations.get(i);
+                //Marker currentDeviceLocation = map.addMarker(new MarkerOptions().position(locations.get(i)[0], locations.get(i)[1]));
+            }
+        }
     }
 }
